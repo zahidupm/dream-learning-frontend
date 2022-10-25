9e2e9697d9a1bf32f700369b3da207808391dc84
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-25 12:51:40 +0600

    user and loading useState

--- src/contexts/auth.context.js
+++ src/contexts/auth.context.js
@@ -1,13 +1,26 @@
-import { getAuth } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../configs/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const authInfo = {
+        user
+    }
+
+    useEffect( () => {
+        const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        })
+    }, [])
+
     return (
-        <AuthContext.Provider value={''}>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
