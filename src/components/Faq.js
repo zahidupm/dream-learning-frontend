00c2e56d339b42a1bc464cd944cb0a8724bf9324
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-25 11:14:11 +0600

    header link setting

---
+++ src/components/Faq.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Faq = () => {
+    return (
+        <div>
+            <h2>Faq</h2>
+        </div>
+    );
+};
+
+export default Faq;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from '../components/Auth/Login';
+import Register from '../components/Auth/Register';
+import Blog from "../components/Blog";
+import Courses from "../components/Courses";
+import Faq from "../components/Faq";
 import Home from "../components/Home";
 import Main from "../layout/Main";
 
@@ -9,7 +14,11 @@ const routes = createBrowserRouter([
         children: [
             {path: '/', element: <Home></Home>},
             {path: '/home', element: <Home></Home>},
-            {path: '/home', element: <Home></Home>},
+            {path: '/login', element: <Login></Login>},
+            {path: '/register', element: <Register></Register>},
+            {path: '/courses', element: <Courses></Courses>},
+            {path: '/faq', element: <Faq></Faq>},
+            {path: '/blog', element: <Blog></Blog>},
         ]
     }
 ])
