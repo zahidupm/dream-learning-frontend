5a12922326e57e241b732fa7cacdf46630dcf4be
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-26 11:34:01 +0600

    add errorpage

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,7 @@ import Login from '../components/Auth/Login';
 import Register from '../components/Auth/Register';
 import Blog from "../components/Blog";
 import Courses from "../components/Courses";
+import ErrorPage from "../components/ErrorPage";
 import Faq from "../components/Faq";
 import Home from "../components/Home";
 import ItemDetails from "../components/ItemDetails";
@@ -12,6 +13,7 @@ const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {path: '/', element: <Home></Home>},
             {path: '/home', element: <Home></Home>},
@@ -24,11 +26,13 @@ const routes = createBrowserRouter([
     {
         path: '/courses', 
         element: <Courses></Courses>,
+        errorElement: <ErrorPage></ErrorPage>,
         loader: () => fetch(`http://localhost:5000/all`),
     }, 
     {
         path: '/item/:id',
         element: <ItemDetails></ItemDetails>,
+        errorElement: <ErrorPage></ErrorPage>,
         loader: ({params}) => {
             return fetch(`http://localhost:5000/item/${params.id}`)
         }
